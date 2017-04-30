3279be35d6260f6aca30bb523121e1d5c042e8bc
Jesse <jesse.matherne@esosolutions.com>
2017-04-30 09:04:44 -0500

    clean up spaces to tabs

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { CourseProjectPage } from './app.po';
 
 describe('course-project App', () => {
-  let page: CourseProjectPage;
+	let page: CourseProjectPage;
 
-  beforeEach(() => {
-    page = new CourseProjectPage();
-  });
+	beforeEach(() => {
+		page = new CourseProjectPage();
+	});
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+	it('should display message saying app works', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toEqual('app works!');
+	});
 });
