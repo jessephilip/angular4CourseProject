81454dd0f46717c00a6c29e5b6e9e0e80df862de
Jesse <jesse.matherne@esosolutions.com>
2017-05-01 07:03:17 -0500

    finished first initial setup.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+	private _ingredients:Ingredient[] = [
+		new Ingredient('Flour', 2),
+		new Ingredient('Salt', 3),
+		new Ingredient('Water', 2)
+	];
+	public get ingredients():Ingredient[] { return this._ingredients; }
+	public set ingredients(ingredients:Ingredient[]) { this._ingredients = ingredients; }
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
